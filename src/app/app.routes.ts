7cea8781ed1a7a8b300778959d58173b0ebfbedf
TheotimALBERTEAU <149970439+TheotimALBERTEAU@users.noreply.github.com>
2025-12-17 17:40:56 +0100

    Add base path going to HomePage

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import {HomePage} from './pages/Movies/home-page/home-page';
 import {MoviePage} from './pages/Movies/movie-page/movie-page';
 
 export const routes: Routes = [
+  {path: '', component: HomePage},
   {path: 'movies', component: HomePage},
   {path: 'movies/:slug', component: MoviePage},
 ];
