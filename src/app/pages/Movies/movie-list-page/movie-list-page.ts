ae9ac1f3003d5a70b0a8bd401f80f6e98c0accbe
TheotimALBERTEAU <149970439+TheotimALBERTEAU@users.noreply.github.com>
2026-04-02 22:46:45 +0200

    ✨ feat : Next/Prev buttons on movie list page

--- src/app/pages/Movies/movie-list-page/movie-list-page.ts
+++ src/app/pages/Movies/movie-list-page/movie-list-page.ts
@@ -1,27 +1,58 @@
-import {ChangeDetectorRef, Component} from '@angular/core';
+import {ChangeDetectorRef, Component, OnInit} from '@angular/core';
 import {MoviesList} from '../../../services/Movies/movies-list';
+import {CommonModule} from '@angular/common';
 
 @Component({
   selector: 'app-movie-list-page',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './movie-list-page.html',
   styleUrl: './movie-list-page.css',
 })
-export class MovieListPage {
+export class MovieListPage implements OnInit {
+  public movies : any[] = []
+  public displayedMovies: any[] = [];
+
+  public currentPage = 1;
+  public pageSize = 32;
+
   constructor(private moviesService: MoviesList,
               private cdr: ChangeDetectorRef) {}
-
-  public movies : any[] = []
 
   ngOnInit() {
     this.moviesService.getMovies().subscribe({
       next: data => {
         if (data.code === "200") {
           this.movies = data.data;
-          this.cdr.detectChanges();
+          this.updateDisplay();
         }
       }
     });
+  }
+
+  updateDisplay() {
+    const start = (this.currentPage - 1) * this.pageSize;
+    const end = start + this.pageSize;
+    this.displayedMovies = this.movies.slice(start, end);
+    this.cdr.detectChanges();
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  }
+
+  nextPage() {
+    if (this.hasNext()) {
+      this.currentPage++;
+      this.updateDisplay();
+    }
+  }
+
+  prevPage() {
+    if (this.currentPage > 1) {
+      this.currentPage--;
+      this.updateDisplay();
+    }
+  }
+
+  hasNext(): boolean {
+    return (this.currentPage * this.pageSize) < this.movies.length;
   }
 
   onClickGoMovie(slug: any) {
