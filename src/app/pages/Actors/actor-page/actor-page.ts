f8862b5997fcacaa3fe332acb0adabb58a44ab3e
TheotimALBERTEAU <149970439+TheotimALBERTEAU@users.noreply.github.com>
2026-04-06 20:07:13 +0200

    ✨ feat : Actor page | actor-page

---
+++ src/app/pages/Actors/actor-page/actor-page.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-actor-page',
+  imports: [],
+  templateUrl: './actor-page.html',
+  styleUrl: './actor-page.css',
+})
+export class ActorPage {
+
+}
