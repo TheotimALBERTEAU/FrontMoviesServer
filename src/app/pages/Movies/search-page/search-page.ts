302707d5ca438ebfc0246dd2dcae0551f06a6b69
TheotimALBERTEAU <149970439+TheotimALBERTEAU@users.noreply.github.com>
2026-04-08 15:38:21 +0200

    ✨ feat : Actors on search | search-page

--- src/app/pages/Movies/search-page/search-page.ts
+++ src/app/pages/Movies/search-page/search-page.ts
@@ -1,40 +1,59 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import { MoviesList} from '../../../services/Movies/movies-list';
 import {ActivatedRoute} from '@angular/router';
 import { ChangeDetectorRef} from '@angular/core';
+import {ActorsList} from '../../../services/Actors/actors-list';
+import {forkJoin} from 'rxjs';
+import {CommonModule} from '@angular/common';
 
 @Component({
   selector: 'app-search-page',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './search-page.html',
   styleUrl: './search-page.css',
 })
-export class SearchPage {
+export class SearchPage implements OnInit {
   public query: string = '';
   public resultsMoviesList: any[] = [];
+  public resultsActorsList: any[] = [];
 
-
-  constructor(private moviesService: MoviesList,
-              private route: ActivatedRoute,
-              private cdr: ChangeDetectorRef) { }
+  constructor(
+    private moviesService: MoviesList,
+    private route: ActivatedRoute,
+    private cdr: ChangeDetectorRef,
+    private actorsService: ActorsList
+  ) {}
 
   ngOnInit() {
-    this.route.params.subscribe(params => {
+    this.route.params.subscribe((params) => {
       this.query = params['query'];
-      this.moviesService.searchMovies(this.query).subscribe(res => {
-        if (res.code === "200") {
-          this.resultsMoviesList = res.data;
-        }
-        this.cdr.detectChanges();
-      });
-    })
+
+      if (this.query) {
+        forkJoin({
+          movies: this.moviesService.searchMovies(this.query),
+          actors: this.actorsService.searchActors(this.query)
+        }).subscribe({
+          next: (res) => {
+            this.resultsMoviesList = res.movies.code === "200" ? res.movies.data : [];
+            this.resultsActorsList = res.actors.code === "200" ? res.actors.data : [];
+            console.log(this.resultsActorsList.length);
+            console.log(this.resultsMoviesList.length);
+
+            this.cdr.detectChanges();
+          },
+          error: (err) => {
+            console.error("Erreur lors de la recherche globale", err);
+          }
+        });
+      }
+    });
   }
 
   onClickGoMovie(slug: any) {
     this.moviesService.goMovie(slug);
   }
 
-  onClickGoGenre(genre: any) {
-    this.moviesService.goGenre(genre.toLowerCase())
+  onClickGoActor(actorSlug: any) {
+    this.actorsService.goActor(actorSlug);
   }
 }
