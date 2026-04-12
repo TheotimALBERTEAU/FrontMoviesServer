b0b3a1e9d1e61b68df0b5ebffb106fe9be85ae2c
Théotim ALBERTEAU <sunshouwu@outlook.com>
2026-04-12 19:54:42 +0200

    🚀 up : clicks system | episode-page

--- src/app/pages/Series/episode-page/episode-page.ts
+++ src/app/pages/Series/episode-page/episode-page.ts
@@ -1,7 +1,9 @@
 import {ChangeDetectorRef, Component, OnInit} from '@angular/core';
 import {WatchSerie} from '../../../services/Series/watch-serie';
-import {ActivatedRoute} from '@angular/router';
+import {ActivatedRoute, Router} from '@angular/router';
 import {NgIf} from '@angular/common';
+import {DomSanitizer, SafeResourceUrl} from '@angular/platform-browser';
+import {MoviesList} from '../../../services/Movies/movies-list';
 
 @Component({
   selector: 'app-episode-page',
@@ -13,11 +15,16 @@ import {NgIf} from '@angular/common';
 })
 export class EpisodePage implements OnInit {
   constructor(private watchSerie: WatchSerie,
+              private moviesService: MoviesList,
               private activatedRoute: ActivatedRoute,
-              private cdr: ChangeDetectorRef) {
+              private cdr: ChangeDetectorRef,
+              private router: Router,
+              private sanitizer: DomSanitizer) {
   }
 
   public episode: any = {};
+  public isTrailerOpen = false;
+  public safeTrailerUrl: SafeResourceUrl | undefined;
 
   ngOnInit() {
     const slug = this.activatedRoute.snapshot.paramMap.get('slug');
@@ -32,5 +39,32 @@ export class EpisodePage implements OnInit {
         }
       })
     }
+  }
+
+  openTrailer() {
+    this.isTrailerOpen = true;
+    let url = this.episode.trailer;
+    if (url.includes('youtube.com/watch?v=')) {
+      url = url.replace('watch?v=', 'embed/');
+    } else if (url.includes('youtu.be/')) {
+      url = url.replace('youtu.be/', 'youtube.com/embed/');
+    }
+
+    const finalUrl = `${url}?autoplay=1&mute=1`;
+    this.safeTrailerUrl = this.sanitizer.bypassSecurityTrustResourceUrl(finalUrl);
+    document.body.style.overflow = 'hidden';
+  }
+
+  closeTrailer() {
+    this.isTrailerOpen = false;
+    document.body.style.overflow = 'auto';
+  }
+
+  onClickGoGenre(genre: any) {
+    this.moviesService.goGenre(genre.toLowerCase())
+  }
+
+  onClickGoActor(actorSlug: any) {
+    this.router.navigate(['/actor/', actorSlug]);
   }
 }
